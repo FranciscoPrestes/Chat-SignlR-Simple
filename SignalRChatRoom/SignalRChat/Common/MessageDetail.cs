fe6c03760e70c998abe212d10d57278842068ea8
Francisco Prestes <franciscoprestes01@gmail.com>
2018-11-14 16:31:42 -0300

    Add Project

---
+++ SignalRChatRoom/SignalRChat/Common/MessageDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRChat.Common
+{
+    public class MessageDetail
+    {
+
+        public string UserName { get; set; }
+
+        public string Message { get; set; }
+    
+    }
+}
---
+++ SignalRChatRoom/SignalRChat/Common/UserDetail.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRChat.Common
+{
+    public class UserDetail
+    {
+        public string ConnectionId { get; set; }
+        public string UserName { get; set; }
+    }
+}
